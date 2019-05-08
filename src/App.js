c08390584e3520aaf95527306dad859359e5707b
rmylonas <roman.mylonas@isb-sib.ch>
2019-05-08 13:20:16 +0200

    put router directly in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ConnectedRouter } from 'connected-react-router'
-import routes from './routes'
+import { Route, Switch } from 'react-router'
+import NavBar from './components/NavBar'
+import ProteinSearchContainer from './components/proteinSearch/ProteinSearchContainer'
+import ProteinViz from './components/proteinViz/ProteinVizContainer'
+import PeptideVizContainer from "./components/peptideViz/PeptideVizContainer";
 
 /*
  TODO use cross-fetch!
@@ -10,7 +14,15 @@ import routes from './routes'
 const App = ({ history }) => {
     return (
         <ConnectedRouter history={history}>
-            { routes }
+            <div id={"routes"}>
+                <NavBar />
+                <Switch>
+                    <Route exact path="/" component={ProteinSearchContainer} />
+                    <Route path="/proteins" component={ProteinViz} />
+                    <Route path="/peptides" component={PeptideVizContainer} />
+                    <Route component={ProteinSearchContainer} />
+                </Switch>
+            </div>
         </ConnectedRouter>
     )
 }
