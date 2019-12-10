a625bb683d2f5b0ea9461bce956e3f2b27692fa1
rmylonas <roman.mylonas@isb-sib.ch>
2019-12-10 11:13:47 +0100

    version 0.3.1

--- src/config.js
+++ src/config.js
@@ -5,10 +5,10 @@
 
 var config = {};
 
-config.version = '0.3.0'
+config.version = '0.3.1'
 
-config.urlBackend = "http://localhost:9000"
-//config.urlBackend = "https://pumba-dev.vital-it.ch/backend"
+//config.urlBackend = "http://localhost:9000"
+config.urlBackend = "https://pumba-dev.vital-it.ch/backend"
 //config.urlBackend = "https://pumba.vital-it.ch/backend"
 
 module.exports = config;
