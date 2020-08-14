6dc2af4da59f11fc30348bbcbdbb773136c3606a
rmylonas <roman.mylonas@isb-sib.ch>
2020-08-14 12:43:31 +0200

    version 0.5.2

--- src/config.js
+++ src/config.js
@@ -5,10 +5,10 @@
 
 var config = {};
 
-config.version = '0.5.1'
+config.version = '0.5.2'
 
-config.urlBackend = "http://localhost:9000"
-//config.urlBackend = "https://pumba-dev.vital-it.ch/backend"
+//config.urlBackend = "http://localhost:9000"
+config.urlBackend = "https://pumba-dev.vital-it.ch/backend"
 //config.urlBackend = "https://pumba.vital-it.ch/backend"
 
 // Gel contrast setting
