e1e7e05a38f9762dbb5fea57d3834d90877e206c
Roman Mylonas <roman.mylonas@sib.swiss>
2021-08-31 08:45:20 +0200

    Add MenuButtonGroup component.

---
+++ src/components/common/MenuButtonGroup.js
@@ -0,0 +1,95 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import ExportSvgButton from "../common/ExportSvgButton";
+import { Button } from "reactstrap";
+import GelOptions from "../options/GelOptions";
+import { GearFill, InfoCircleFill } from "react-bootstrap-icons";
+import GelHelpModal from "../gelViz/GelHelpModal";
+
+class MenuButtonGroup extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showHelpModal: false,
+      showOptionsMenu: false,
+    };
+  }
+
+  optionsButton() {
+    const { selectedViz } = this.props;
+
+    if (selectedViz === "lanes") {
+      return <GelOptions></GelOptions>;
+    }
+  }
+
+  helpModal() {
+    const { selectedViz } = this.props;
+    const { showHelpModal } = this.state;
+
+      if (selectedViz === "lanes") {
+        return (
+          <GelHelpModal
+            toggle={this.toggleHelp}
+            modal={showHelpModal}
+          ></GelHelpModal>
+        );
+      }
+  }
+
+  fileName() {
+    const { selectedViz, proteinId } = this.props;
+    return proteinId + "-" + selectedViz;
+  }
+
+  toggleHelp = () => {
+    this.setState({ showHelpModal: !this.state.showHelpModal });
+  };
+
+  toggleOptions = () => {
+    this.setState({ showOptionsMenu: !this.state.showOptionsMenu });
+  };
+
+  render() {
+    const { svg } = this.props;
+    const { showOptionsMenu } = this.state;
+
+    return (
+      <div id={"gel-option-group"}>
+        <Button
+          active={showOptionsMenu}
+          color="primary"
+          onClick={() => this.toggleOptions()}
+        >
+          <span>
+            <GearFill />
+          </span>
+          &nbsp; Options
+        </Button>
+        &nbsp;
+        {showOptionsMenu && this.optionsButton()}
+        <ExportSvgButton
+          svg={svg}
+          fileName={this.fileName()}
+        ></ExportSvgButton>
+        &nbsp;
+        <Button color="primary" onClick={() => this.toggleHelp()}>
+          <span>
+            <InfoCircleFill />
+          </span>
+          &nbsp; Help
+        </Button>
+        { this.helpModal() }
+      </div>
+    );
+  }
+}
+
+MenuButtonGroup.propTypes = {
+  selectedViz: PropTypes.string.isRequired,
+  svg: PropTypes.object.isRequired,
+  proteinId: PropTypes.object.isRequired,
+};
+
+export default MenuButtonGroup;
