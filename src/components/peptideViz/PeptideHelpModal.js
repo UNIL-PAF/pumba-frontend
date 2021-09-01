766a3816cd725f11c5090605d10d3352a7d0ca0f
Roman Mylonas <roman.mylonas@sib.swiss>
2021-09-01 21:28:09 +0200

    Add protein and peptide help.

--- src/components/peptideViz/PeptideHelpModal.js
+++ src/components/peptideViz/PeptideHelpModal.js
@@ -9,16 +9,24 @@ class PeptideHelpModal extends PureComponent {
 
     const items = [
       {
-        title: "Peptide Help",
-        src: "/images/help_movies/gel_click_title.gif",
+        title: "Zoom into region of interest",
+        src: "/images/help_movies/peptide_zoom.gif",
         text: [
-          "You can open the selected protein in a new browser tab by clicking on the title.",
+          "You can zoom into a region of the peptide graph by clicking and holding the left mouse button, and thus determing the new plot area.",
+          "To zoom out again you just have to double click on any place within the graph.",
+        ],
+      },
+      {
+        title: "Data selection",
+        src: "/images/help_movies/peptide_data_selection.gif",
+        text: [
+          "In the legend you can hide whole cell lines or remove a single replicate from the graph.",
         ],
       },
     ];
 
     return (
-      <HelpModal items={items} modal={modal} toggle={toggle} title={"Lanes help"}>
+      <HelpModal items={items} modal={modal} toggle={toggle} title={"Peptides help"}>
       </HelpModal>
     );
   }
--- src/components/proteinViz/ProteinHelpModal.js
+++ src/components/proteinViz/ProteinHelpModal.js
@@ -9,16 +9,35 @@ class ProteinHelpModal extends PureComponent {
 
     const items = [
       {
-        title: "Protein Help",
-        src: "/images/help_movies/gel_click_title.gif",
+        title: "Zoom into graph",
+        src: "/images/help_movies/protein_zoom.gif",
         text: [
-          "You can open the selected protein in a new browser tab by clicking on the title.",
+          "You can zoom into the graph by clicking on the left mouse button and dragging while holding down the button. You have to release the button when you reached the desired position.",
+          "To zoom out again, you simply double click on any place in the graph.",
+        ],
+      },
+      {
+        title: "Zoom into low intensity region",
+        src: "/images/help_movies/protein_intensity_zoom.gif",
+        text: [
+          "When looking at a region with low intensities it can be useful adapt the zoom for the intensity (y-axis).",
+          "You zoom by adapting the maximum intensity from the options menu.",
+          "There is a reset button, to go back to the inital state.",
+        ],
+      },
+      {
+        title: "Data selection",
+        src: "/images/help_movies/protein_data_selection.gif",
+        text: [
+          "In the legend you can hide whole cell lines or remove a single replicate from the graph.",
+          "By moving the mouse pointer over the replicates you can see the individual intensities for each slice. The final curve for each cell line is created by an interpolation of those slice intensities.",
+          "When you click on the name of a replicate, you can fix it and then get more information (e.g. number of peptides) for each slice by moving the mouse pointer on it."
         ],
       },
     ];
 
     return (
-      <HelpModal items={items} modal={modal} toggle={toggle} title={"Lanes help"}>
+      <HelpModal items={items} modal={modal} toggle={toggle} title={"Graph help"}>
       </HelpModal>
     );
   }
